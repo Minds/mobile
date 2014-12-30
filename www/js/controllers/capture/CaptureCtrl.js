e601a7581a8694e84e861332f3c3b00bdbb62623
Mark Harding <mark@kramnorth.com>
2014-12-30 13:10:45 +0000

    implements video upload

--- www/js/controllers/capture/CaptureCtrl.js
+++ www/js/controllers/capture/CaptureCtrl.js
@@ -8,27 +8,30 @@
 define(function () {
     'use strict';
 
-    function ctrl($scope, $stateParams, $rootScope, Client, OAuth, storage, $ionicModal) {
+    function ctrl($scope, $stateParams, $state, $rootScope, Client, OAuth, storage, $ionicModal) {
     
     	$scope.captured = false;
+    	$scope.progress = 0;
 
 		$scope.video = function(){
 			navigator.device.capture.captureVideo(function(mediaFiles){
-				console.log('capture complete');
+				
+				$scope.captured = true;
+				$scope.$apply();
+			
 				var i, path, len;
 			    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
 			    	var mediafile = mediaFiles[i];
 			        path = mediafile.fullPath;
-			        console.log(path);
-			        
-			        var options = new FileUploadOptions();
-			        var ft = new FileTransfer();
-			        
-			    //    ft.upload(path, encodeURI($rootScope.node_url + 'api/v1/archive'), function(success){console.log(success);}, function(error){console.log(error);}, OAuth.buildParams(options));
+
+			       	$scope.upload(path, 'video');
 			    }
 			}, function(){
 				console.log('capture failed');
-			}, {limit: 2, duration:30});
+			}, {
+				limit: 2, 
+				duration:30
+			});
 		};
 		
 		$scope.photo= function(){
@@ -41,43 +44,71 @@ define(function () {
 				var i, path, len;
 			    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
 			        path = mediaFiles[i].fullPath;
-			        
-			        var ft = new FileTransfer();
-			        var options = new FileUploadOptions();
-			        options.httpMethod = 'PUT';
-			        options.headers = {"Authorization": "Bearer " + storage.get('access_token') };
-			       	
-			      	ft.upload(path, encodeURI($rootScope.node_url + 'api/v1/archive'), 
-			      		function(success){
-			      			var response = JSON.parse(success.response);
-			      			$scope.guid = response.guid;
-			      			$scope.$apply();
-			      		}, 
-			      		function(error){
-			      			console.log('error');
-			      			console.log(error);
-			      		}, 
-			      		options
-			      	);
+			       
+			        $scope.upload(path, 'image');
  
 			    }
 			}, function(){
 				console.log('capture failed');
 				alert('Uploading to Minds will be here soon!');
-			}, {limit: 1});
+			}, {
+				limit: 1,
+				correctOrientation: true,
+				ecodingType: Camera.EncodingType.JPEG,
+				targetWidth: 720
+			});
 		};
 		
 		$scope.reset = function(){
 			$scope.captured = false;
+			$scope.guid = false;
+		};
+		
+		$scope.upload = function(path, type){
+			var ft = new FileTransfer();
+	        var options = new FileUploadOptions();
+	        options.httpMethod = 'PUT';
+	        options.headers = {"Authorization": "Bearer " + storage.get('access_token') };
+	        console.log(path);
+	      	ft.upload(path, encodeURI($rootScope.node_url + 'api/v1/archive/'+type), 
+	      		function(success){
+	      			if(success.response){
+		      			var response = JSON.parse(success.response);
+		      			
+		      			$scope.guid = response.guid;
+		      			$scope.$apply();
+	      			}
+	      		}, 
+	      		function(error){
+	      			console.log('error');
+	      			console.log(error);
+	      		}, 
+	      		options
+	      	);
+	      	
+	      	ft.onprogress = function(progressEvent) {
+	      		$scope.progress =  Math.floor(progressEvent.loaded / progressEvent.total * 100);
+	      		$scope.$apply();
+			}; 
 		};
 		
 		$scope.save = function(){
-			
+			Client.post('api/v1/archive/' + $scope.guid, {
+					album_title: 'Mobile'
+				},
+				function(success){
+					alert('done, a future build will take you to the image view..');
+					$scope.reset();
+				}, 
+				function(error){
+					alert('oops. something didn\'t go to plan.');
+					$scope.reset();
+				});
 		};
        
     }
 
-    ctrl.$inject = ['$scope', '$stateParams', '$rootScope', 'Client', 'OAuth', 'storage', '$ionicModal'];
+    ctrl.$inject = ['$scope', '$stateParams', '$state', '$rootScope', 'Client', 'OAuth', 'storage', '$ionicModal'];
     return ctrl;
     
 });

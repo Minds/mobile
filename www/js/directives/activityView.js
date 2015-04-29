68fdf88beca39aadcf0c2b0ecee62f858b0193fa
Mark Harding <mark@kramnorth.com>
2015-04-29 16:50:40 +0100

    activity now detects if its in view and send view view metric if so

--- www/js/directives/activityView.js
+++ www/js/directives/activityView.js
@@ -3,15 +3,44 @@
 define(['angular'], function (angular) {
     "use strict";
 
-    var directive = function () {
+    var directive = function ($ionicScrollDelegate, $ionicGesture, Client) {
 	 	return {
        		restrict: 'E',
 			templateUrl: function(elem, attr){
 				return 'templates/directives/activity.html';
+			},
+			link: function(scope, element, attrs){
+			
+				var visible = false;
+			
+				function isVisible(){
+				    return (element[0].offsetTop - 200 <= $ionicScrollDelegate.getScrollPosition().top);
+			   	}
+			   	
+			   	if(isVisible()){
+			   		visible = true;
+			   		updateViews();
+			   	}
+
+				if(!visible){
+				 	var gesture = $ionicGesture.on('dragend', function(e){
+				 		if(!visible && isVisible()){
+				   			visible = true;
+				   			$ionicGesture.off(gesture, 'dragend', this);
+				   			updateViews();
+				 	  	}
+				 	}, element.parent());
+			 	}
+			 	
+			 	function updateViews(){
+			 		scope.activity.impressions = scope.activity.impressions + 1;
+			 		Client.put('api/v1/newsfeed/' + scope.activity.guid + '/view',{}, function(){}, function(){});
+			 	}
+			 
 			}
        	 };
     };
 
-    directive.$inject = [];
+    directive.$inject = ['$ionicScrollDelegate', '$ionicGesture', 'Client'];
     return directive;
 });

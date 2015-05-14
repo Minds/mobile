2128d8f1bcd368f55e63268fcd7978183dd09b12
Mark Harding <mark@kramnorth.com>
2015-05-14 08:38:06 +0100

    fixes for travis

--- karma.conf.js
+++ karma.conf.js
@@ -69,10 +69,22 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
+    
+    
+	customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
+  
+  if(process.env.TRAVIS){
+	config.browsers = ['Chrome_travis_ci'];
+  }
+    
 };

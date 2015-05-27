b872efe20e446491a744cfc591de40cebf0e3aa8
Mark Harding <mark@kramnorth.com>
2015-05-27 15:14:22 +0100

    adds tests to channel edit

--- tests/spec/controllers/ChannelSpec.js
+++ tests/spec/controllers/ChannelSpec.js
@@ -139,4 +139,134 @@ define(['angular', 'angular-mocks', 'app'], function(angular, mocks, app) {
 
 	});
 
+	describe('Channel Edit (ChannelEditCtrl)', function() {
+
+		var rootScope, scope, httpBackend, timeout, state, ionicLoading, ionicPopup, q, _storage, ionicHistory;
+
+		/*
+		 * Setup
+		 */
+		beforeEach(module('ionic'));
+		beforeEach(module('app.controllers'));
+
+		beforeEach(inject(function($rootScope, $controller, $timeout, $state, $ionicLoading, $ionicPopup, $q, $stateParams, storage, $ionicHistory) {
+			rootScope = $rootScope;
+			scope = $rootScope.$new();
+			$controller('ChannelEditCtrl', {
+				$scope: scope,
+				$stateParams: { guid: "001" }
+			});
+
+			timeout = $timeout,
+			state = $state,
+			ionicLoading = $ionicLoading,
+			ionicPopup = $ionicPopup,
+			q = $q,
+			_storage = storage,
+			ionicHistory = $ionicHistory;
+		}));
+
+		beforeEach(inject(function($httpBackend) {
+			httpBackend = $httpBackend;
+
+			var result = {
+				"status": "success",
+				"channel": {
+					guid: "001",
+					name: "karma",
+					username: "karma",
+					city: "cyberspace",
+					coordinates: "52.0,-2.0"
+				}
+			};
+
+			httpBackend.when('GET', /.*\/channel\.*/).respond(result);
+			httpBackend.flush();
+		}));
+
+		beforeEach(inject(function($httpBackend) {
+			httpBackend = $httpBackend;
+
+			var result = {
+				"status": "success"
+			};
+
+			httpBackend.when('POST', /.*\/channel\/.*/).respond(result);
+		}));
+
+		/**
+		 * Tests
+		 */
+
+		it('can not update without a channel name', function() {
+
+			spyOn(ionicPopup, "alert");
+
+			scope.channel.name = "";
+
+			expect(scope.update()).toEqual(false);
+			expect(ionicPopup.alert).toHaveBeenCalledWith({
+					title: 'Oooops...',
+					template: "Your need a name!"
+				});
+		});
+
+		it('will save and forward', function() {
+
+			spyOn(state, "go");
+
+			scope.update(true);
+			httpBackend.flush();
+
+			expect(state.go).toHaveBeenCalledWith('tab.newsfeed-channel', {
+						username: 'me',
+						refresh: true
+					});
+		});
+
+		it('can disable account', function() {
+
+			spyOn(scope, "logout");
+
+			//listen for request
+			inject(function($httpBackend) {
+				httpBackend = $httpBackend;
+
+				var result = { "status": "success" };
+
+				httpBackend.when('DELETE', /.*\/channel\.*/).respond(result);
+			});
+
+			scope.disable();
+			httpBackend.flush();
+			expect(scope.logout).toHaveBeenCalledWith();
+		});
+
+		it('can lgoout', function() {
+
+			spyOn(state, "go");
+			spyOn(ionicHistory, "clearCache");
+
+			//listen for request
+			inject(function($httpBackend) {
+				httpBackend = $httpBackend;
+
+				var result = { "status": "success" };
+
+				httpBackend.when('POST', /.*\/logout\.*/).respond(result);
+			});
+
+			scope.logout();
+			httpBackend.flush();
+
+			expect(_storage.get('loggedin')).toEqual(false);
+			expect(_storage.get('access_token')).toEqual(false);
+			expect(_storage.get('user_guid')).toEqual(false);
+
+			expect(ionicHistory.clearCache).toHaveBeenCalledWith();
+			expect(state.go).toHaveBeenCalledWith('login');
+		});
+
+	});
+
 });

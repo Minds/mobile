5fb3f30695338ba5a0a9bb45aa9cfe3cfcf82875
Mark Harding <mark@kramnorth.com>
2017-01-25 16:26:41 +0000

    (feat): can now edit messages

--- src/app/modules/newsfeed/activity/activity.component.ts
+++ src/app/modules/newsfeed/activity/activity.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, OnDestroy, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ChangeDetectionStrategy, ChangeDetectorRef, HostListener } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular'
 import { CacheService } from '../../../common/services/cache/cache.service';
+import { Storage } from '../../../common/services/storage';
+import { Client } from '../../../common/services/api/client';
 import { ChannelComponent } from '../../channel/channel.component';
 
 
@@ -15,6 +18,7 @@ import { ChannelComponent } from '../../channel/channel.component';
 export class Activity {
 
   entity;
+  editing : boolean = false;
 
   minds = {
     cdn_url: 'https://edge.minds.com/'
@@ -24,8 +28,10 @@ export class Activity {
     channel: ChannelComponent
   }
 
+  storage = new Storage();
 
-  constructor(public cache : CacheService){
+  constructor(private client : Client, public cache : CacheService, public actionSheetCtrl: ActionSheetController,
+    private cd : ChangeDetectorRef){
 
   }
 
@@ -43,6 +49,72 @@ export class Activity {
 
   openInBrowser(url){
     (<any>window).open(url, "_system");
+  }
+
+  @HostListener('press', ['$event'])
+  openSettings(e){
+    let buttons = [];
+
+    if(this.storage.get('user_guid') == this.entity.owner_guid){
+      buttons.push({
+        text: 'Delete',
+        role: 'destructive',
+        handler: () => {
+          //console.log('Destructive clicked');
+        }
+      });
+      buttons.push({
+        text: 'Edit',
+        handler: () => {
+          this.editing = true;
+          this.cd.markForCheck();
+          this.cd.detectChanges();
+          //console.log('Destructive clicked');
+        }
+      });
+    }
+
+    buttons.push({
+      text: 'Translate',
+      handler: () => {
+       console.log('Translate clicked');
+      }
+    });
+
+    buttons.push({
+      text: 'Share',
+      handler: () => {
+       console.log('Share clicked');
+      }
+    });
+
+    buttons.push({
+      text: 'Report',
+      handler: () => {
+       console.log('Report clicked');
+      }
+    });
+
+    buttons.push({
+      text: 'Cancel',
+      role: 'cancel'
+    });
+
+    let actionSheet = this.actionSheetCtrl.create({
+      //title: '',
+      buttons: buttons
+    });
+    actionSheet.present();
+  }
+
+  save(){
+    this.client.post('api/v1/newsfeed/' + this.entity.guid, {
+      message: this.entity.message,
+      title: this.entity.title
+    });
+    this.editing = false;
+    this.cd.markForCheck();
+    this.cd.detectChanges();
   }
 
 

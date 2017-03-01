cf383d3a6088774cb7bc914f127292517be5b7e8
Mark Harding <mark@kramnorth.com>
2017-03-01 13:10:35 +0000

    (fix): add rich input to exports of common module

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -3,17 +3,19 @@ import { IonicModule } from 'ionic-angular';
 
 import { Storage } from './services/storage';
 import { ImgFadeDirective } from './directives/img-fade.directive';
+import { ExplicitDirective } from './directives/explicit.directive';
 import { ContenteditableModel } from './directives/content-editable-model.directive.ts';
 import { AbbrPipe } from './pipes/abbr.pipe';
 import { DomainPipe } from './pipes/domain.pipe';
 import { TagsPipe } from './pipes/tags.pipe';
 import { TextareaComponent } from './components/textarea.component';
+import { RichInputComponent } from './components/rich-input.component.ts';
 
 
 @NgModule({
   imports: [ IonicModule ],
   providers: [ Storage ],
-  declarations: [ ImgFadeDirective, ContenteditableModel, DomainPipe, AbbrPipe, TagsPipe, TextareaComponent ],
-  exports: [ ImgFadeDirective, ContenteditableModel, DomainPipe, AbbrPipe, TagsPipe, TextareaComponent ]
+  declarations: [ ImgFadeDirective, ExplicitDirective, ContenteditableModel, DomainPipe, AbbrPipe, TagsPipe, TextareaComponent, RichInputComponent ],
+  exports: [ ImgFadeDirective, ExplicitDirective, ContenteditableModel, DomainPipe, AbbrPipe, TagsPipe, TextareaComponent, RichInputComponent ]
 })
 export class CommonModule { }

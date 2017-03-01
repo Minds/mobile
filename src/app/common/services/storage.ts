aaa4aca8448d4408cd465f081b2c07765a036b34
Mark Harding <mark@kramnorth.com>
2017-03-01 13:10:07 +0000

    (fix): json encode storage to work with old app

--- src/app/common/services/storage.ts
+++ src/app/common/services/storage.ts
@@ -1,13 +1,25 @@
 import {Injectable} from '@angular/core';
 
 export class Storage {
+
 	get(key : string) : boolean | string{
-		return window.localStorage.getItem(key);
+		let val = window.localStorage.getItem(key);
+		if(val){
+			try {
+				return JSON.parse(val);
+			} catch (e) {
+				return val;
+			}
+		}
+		return null;
 	}
+
 	set(key : string, value : any){
-		return window.localStorage.setItem(key, value);
+		return window.localStorage.setItem(key, JSON.stringify(value));
 	}
+
 	destroy(key : string){
 		return window.localStorage.removeItem(key);
 	}
+
 }

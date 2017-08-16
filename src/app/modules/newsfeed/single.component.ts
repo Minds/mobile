5196a36eaedd7f038301a104b95e0a9828dafad4
Emi Balbuena <edgebal@gmail.com>
2017-08-16 14:23:47 -0300

    (feat): "smart" single newsfeed view, redirects to proper media if not activity

--- src/app/modules/newsfeed/single.component.ts
+++ src/app/modules/newsfeed/single.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { NavParams, NavController } from 'ionic-angular';
+import { NavParams, NavController, App } from 'ionic-angular';
 import { Client } from '../../common/services/api/client';
+import { BlogView } from "../blog/view.component";
+import { DiscoveryView } from "../discovery/view.component";
 
 
 @Component({
@@ -15,7 +17,7 @@ export class NewsfeedSingleComponent {
   entity;
   inProgress : boolean = false;
 
-  constructor(private navCtrl : NavController, private client : Client, private params: NavParams, private cd : ChangeDetectorRef){}
+  constructor(private navCtrl : NavController, private client : Client, private params: NavParams, private cd : ChangeDetectorRef, private app: App){}
 
   ngOnInit(){
     this.guid = this.params.get('guid');
@@ -27,6 +29,24 @@ export class NewsfeedSingleComponent {
     this.client.get('api/v1/newsfeed/single/' + this.guid, {})
       .then((response : any) => {
         this.entity = response.activity;
+
+        if (this.entity.type != 'activity') {
+          let navTo;
+
+          if (this.entity.subtype === 'blog') {
+            navTo = this._navTo(BlogView, { guid: this.entity.guid }, { animate: false });
+          } else if (this.entity.type === 'object') {
+            navTo = this._navTo(DiscoveryView, { guid: this.entity.guid }, { animate: false });
+          }
+
+          navTo
+            .then(() => {
+              this.navCtrl.remove(this.navCtrl.length() - 2); // remove this view from stack
+            });
+
+          return;
+        }
+
         this.cd.markForCheck();
         this.cd.detectChanges();
       });
@@ -43,4 +63,7 @@ export class NewsfeedSingleComponent {
     return true;
   }
 
+  private _navTo(...args): Promise<any> {
+    return this.app.getActiveNav().push.apply(this.app.getActiveNav(), args);
+  }
 }

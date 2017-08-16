be9819cb4bfb81941906daf134c542cdf6439860
Marcelo <malrogf@gmail.com>
2017-08-16 09:49:12 -0300

    (feat): move new billing section over to mobile (#85)

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,13 +1,21 @@
-import { NgModule }     from '@angular/core';
+import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 
 import { CommonModule } from '../../common/common.module';
 import { SettingsComponent } from './settings.component';
+import { SettingsBillingSavedCardsComponent } from './billing/saved-cards/saved-cards.component';
+import { SettingsBillingSubscriptionsComponent } from './billing/subscriptions/subscriptions.component';
+import { PaymentsModule } from '../payments/payments.module';
 
 @NgModule({
-  imports: [ IonicModule, CommonModule ],
-  declarations: [ SettingsComponent ],
+  imports: [ IonicModule, CommonModule, PaymentsModule ],
+  declarations: [
+    SettingsComponent,
+    SettingsBillingSavedCardsComponent,
+    SettingsBillingSubscriptionsComponent
+  ],
   exports: [ SettingsComponent ],
   entryComponents: [ SettingsComponent ]
 })
-export class SettingsModule { }
+export class SettingsModule {
+}
